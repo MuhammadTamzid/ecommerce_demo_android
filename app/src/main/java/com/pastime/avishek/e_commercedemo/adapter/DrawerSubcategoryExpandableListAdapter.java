d7afed6cedbc8f60bbb2101ff18be0606257d23b
Avishek Khan <Avishek@Framgia-2.local>
2017-05-04 15:23:12 +0600

    Bugfix in DrawerSubcategoryExpandableListAdapter.java and Added up and down arrow icons

--- app/src/main/java/com/pastime/avishek/e_commercedemo/adapter/DrawerSubcategoryExpandableListAdapter.java
+++ app/src/main/java/com/pastime/avishek/e_commercedemo/adapter/DrawerSubcategoryExpandableListAdapter.java
@@ -15,12 +15,14 @@ import com.pastime.avishek.e_commercedemo.interfaces.DrawerSubmenuListener;
 import java.util.List;
 import java.util.Map;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by Avishek on 4/18/17.
  */
 
 public class DrawerSubcategoryExpandableListAdapter extends BaseExpandableListAdapter {
-    private Context mContext;
     private List<String> mExpandableListTitle;
     private Map<String, List<String>> mExpandableListDetail;
     private LayoutInflater mLayoutInflater;
@@ -29,10 +31,9 @@ public class DrawerSubcategoryExpandableListAdapter extends BaseExpandableListAd
     public DrawerSubcategoryExpandableListAdapter(Context context, List<String> expandableListTitle,
                                                   Map<String, List<String>> expandableListDetail,
                                                   DrawerSubmenuListener drawerSubmenuListener) {
-        mContext = context;
         mExpandableListTitle = expandableListTitle;
         mExpandableListDetail = expandableListDetail;
-        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context
                 .LAYOUT_INFLATER_SERVICE);
         mDrawerSubmenuListener = drawerSubmenuListener;
     }
@@ -52,12 +53,15 @@ public class DrawerSubcategoryExpandableListAdapter extends BaseExpandableListAd
     public View getChildView(final int listPosition, final int expandedListPosition,
                              boolean isLastChild, View convertView, ViewGroup parent) {
         final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        ChildViewHolder viewHolder;
         if (convertView == null) {
             convertView = mLayoutInflater.inflate(R.layout.list_item, null);
+            viewHolder = new ChildViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ChildViewHolder) convertView.getTag();
         }
-        TextView expandedListTextView = (TextView) convertView
-                .findViewById(R.id.expandedListItem);
-        expandedListTextView.setText(expandedListText);
+        viewHolder.expandedListTextView.setText(expandedListText);
         convertView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,50 +96,20 @@ public class DrawerSubcategoryExpandableListAdapter extends BaseExpandableListAd
     @Override
     public View getGroupView(final int listPosition, boolean isExpanded,
                              View convertView, ViewGroup parent) {
-        String listTitle = (String) getGroup(listPosition);
+        GroupViewHolder viewHolder;
         if (convertView == null) {
-            convertView = mLayoutInflater.inflate(R.layout.list_group, null);
-        }
-        TextView listTitleTextView = (TextView) convertView
-                .findViewById(R.id.listTitle);
-        ImageView imageIndicator = (ImageView) convertView.findViewById(R.id
-                .image_drawer_subcategory_indicator);
-        listTitleTextView.setTypeface(null, Typeface.BOLD);
-        listTitleTextView.setText(listTitle);
-        if (isExpanded) {
-            imageIndicator.setImageResource(android.R.drawable.arrow_up_float);
-        } else {
-            imageIndicator.setImageResource(android.R.drawable.arrow_down_float);
-        }
-        if (getChildrenCount(listPosition) == 0) {
-            imageIndicator.setVisibility(View.GONE);
-            convertView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mDrawerSubmenuListener.onSubmenuGroupClicked(listPosition, (String) getGroup
-                            (listPosition));
-                }
-            });
-        }
-        return convertView;
-
-        // TODO:
-        /*String listTitle = (String) getGroup(listPosition);
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            viewHolder =  new ViewHolder(mLayoutInflater.inflate(R.layout.list_group,
-                    null));
-            convertView = mLayoutInflater.inflate(R.layout.list_group, null);
+            convertView = mLayoutInflater.inflate(R.layout.list_group, parent, false);
+            viewHolder = new GroupViewHolder(convertView);
             convertView.setTag(viewHolder);
         } else {
-            viewHolder = (ViewHolder) convertView.getTag();
+            viewHolder = (GroupViewHolder) convertView.getTag();
         }
         viewHolder.listTitleTextView.setTypeface(null, Typeface.BOLD);
-        viewHolder.listTitleTextView.setText(listTitle);
+        viewHolder.listTitleTextView.setText((String) getGroup(listPosition));
         if (isExpanded) {
-            viewHolder.imageIndicator.setImageResource(android.R.drawable.arrow_up_float);
+            viewHolder.imageIndicator.setImageResource(R.drawable.arrow_up_black);
         } else {
-            viewHolder.imageIndicator.setImageResource(android.R.drawable.arrow_down_float);
+            viewHolder.imageIndicator.setImageResource(R.drawable.arrow_down_black);
         }
         if (getChildrenCount(listPosition) == 0) {
             viewHolder.imageIndicator.setVisibility(View.GONE);
@@ -147,16 +121,37 @@ public class DrawerSubcategoryExpandableListAdapter extends BaseExpandableListAd
                 }
             });
         }
-        return convertView;*/
+        return convertView;
     }
 
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isChildSelectable(int listPosition, int expandedListPosition) {
         return true;
+    }
+
+    public class GroupViewHolder {
+
+        @BindView(R.id.listTitle)
+        TextView listTitleTextView;
+        @BindView(R.id.image_drawer_subcategory_indicator)
+        ImageView imageIndicator;
+
+        public GroupViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    public class ChildViewHolder {
+        @BindView(R.id.expandedListItem)
+        TextView expandedListTextView;
+
+        public ChildViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
